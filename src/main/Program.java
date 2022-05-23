cf77f39d7b4ed02985161fedbcffd3de4ad37939
WobblyDugong <banhidibotond@gmail.com>
2022-05-23 15:48:20 +0200

    hibákkal működik

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,8 +1,44 @@
 package main;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Program {
     
     public static void main(String[] args) {
-        
+        System.out.println("Torpedó játék");
+        int[] hely = new int[3];
+        int i = 0;
+        while(hely.length!=i) {
+            int rnd = (int)(Math.random() * 7) + 1;
+            hely[i] = rnd;
+            i++;
+        }
+        Hajo hajo = new Hajo(hely);
+        boolean sullyedt = false;
+        int találat = 0;
+        do {
+            if(hajo.talalat(jatekosLoves()).equals("talált")){
+                találat++;
+                System.out.println("talált");
+            }else{
+                System.out.println("nem talált");
+            }
+            if(találat==3){
+                sullyedt=true;
+                System.out.println("A hajó elsüllyedt");
+            }
+        } while (!sullyedt);
+//        
     }
+
+    
+    public static int jatekosLoves(){
+        Scanner src = new Scanner(System.in);
+        System.out.println("Tüzelj: ");
+        return src.nextInt();
+    }
+    
+    
+    
 }
